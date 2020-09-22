81f4ec0e5d150d80bd7d397b633e17deae427759
sunys <sunyuansheng@grgbanking.com>
2020-09-22 10:37:54 +0800

    :sparkles: feat(ClientLicenseManager):
    ClientLicenseManager verify

--- licenseClient/src/main/java/com/syshlang/license/client/core/ClientLicenseManagerHolder.java
+++ licenseClient/src/main/java/com/syshlang/license/client/core/ClientLicenseManagerHolder.java
@@ -12,11 +12,14 @@ package com.syshlang.license.client.core;
 import de.schlichtherle.license.LicenseContentException;
 import de.schlichtherle.license.LicenseManager;
 
+/**
+ * @author sunys
+ */
 public class ClientLicenseManagerHolder {
 
     private static volatile LicenseManager  CLIENT_LICENSE_MANAGER;
 
-    public static LicenseManager getInstance() throws LicenseContentException {
+    public static LicenseManager getInstance() {
         if(CLIENT_LICENSE_MANAGER == null){
             synchronized (ClientLicenseManagerHolder.class){
                 if(CLIENT_LICENSE_MANAGER == null){
